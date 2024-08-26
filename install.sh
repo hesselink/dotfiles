5d67c231d22c0e5790cb422b6756df77222d31d8
Erik Hesselink <hesselink@gmail.com>
2024-08-26 12:16:42 +0200

    Add force parameter to install script

--- install.sh
+++ install.sh
@@ -15,13 +15,37 @@ elem()
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 me="$(basename "${BASH_SOURCE[0]}")"
 ignores=". .. $me .git .gitmodules"
+force=
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -f|--force)
+      force=YES
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      echo "Trailing argument $1"
+      exit 1
+      ;;
+  esac
+done
 
 for file in $(ls -a $dir); do
   if elem "$file" "$ignores"; then
     # echo $file ignored.
     continue
   elif [ -e $HOME/$file ]; then
-    echo $file exists, skipping.
+    if [ $force = "YES" ]; then
+      echo $file exists and force specified, overwriting. Backup saved to $file.bak
+      mv "$HOME/$file" "$HOME/$file.bak"
+      ln -s "$dir/$file" "$HOME/$file"
+    else
+      echo $file exists, skipping.
+    fi
   else
     ln -s "$dir/$file" "$HOME/$file"
   fi
